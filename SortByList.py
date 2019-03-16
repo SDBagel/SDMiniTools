61e71e8c1ef77252ae1a4619155175f62394872c
Kyle <35588499+SDBagel@users.noreply.github.com>
2019-03-15 21:48:35 -0700

    Create SortByList.py
    Takes list, does things, sorts files. Very cool, thank you kanye.

---
+++ SortByList.py
@@ -0,0 +1,25 @@
+import glob
+import shutil
+
+def SortByList(listPath, fileExtension, originFolderPath, destinationFolderPath):
+    files = glob.glob(originFolderPath+"\\*."+fileExtension)
+    list = open(listPath, "r").read()
+
+    for file in files:
+        filename = file.split('\\')[1][0:-(len(fileExtension)+1)]
+        print(filename)
+        if (filename in list):
+            print("true")
+            oldpath = originFolderPath+'\\'+filename+"."+fileExtension
+            newpath = destinationFolderPath+'\\'+filename+"."+fileExtension
+            shutil.move(oldpath, newpath)
+
+    movedFiles = glob.glob(destinationFolderPath+"\\*."+fileExtension)
+    print("Operation Completed: "+str(len(movedFiles))+" out of "+str(len(list.split("\n")))+" were moved to the destination folder.")
+    input()
+
+lP = input("ListPath >")
+fE = input("FileExtension >")
+oFP = input("Origin Folder >")
+dFP = input("Destination Folder >")
+SortByList(lP, fE, oFP, dFP)
