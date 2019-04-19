b7924803ff318e68342cee233fcdf1661ff736fa
Kyle <35588499+SDBagel@users.noreply.github.com>
2019-04-19 11:42:27 -0700

    Fixed positional arguments exception

--- HTMLMinifier.py
+++ HTMLMinifier.py
@@ -1,20 +1,25 @@
 class MinifyHTML(object):
     def __init__(self, htmlFilename, escapeCharacters):
-        minifiedHTML = self.minify(open(htmlFilename, 'r').readlines())
+        htmlLines = open(htmlFilename, 'r').readlines()
+        minifiedHTML = self.minify(htmlLines)
         if (escapeCharacters):
-            print(escape(minifiedHTML))
+            print("")
+            print(self.escape(minifiedHTML))
         else:
+            print("")
             print(minifiedHTML)
 
-    def minify(htmlLines):
+    def minify(self, htmlLines):
         result = ""
         for line in htmlLines:
             result += line.strip()
         return result
 
-    def escape(html):
-        minifiedHTML.replace("'", "\\'")
-        minifiedHTML.replace('"', '\\"')
+    def escape(self, html):
+        html = html.replace("'", "\\'")
+        html = html.replace('"', '\\"')
+        return html
 
 while(True):
     MinifyHTML(input("Filename > "), bool(input("Escape Characters? [True/False] > ")))
+    print("")
